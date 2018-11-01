9ba8af71baac1991cb7a17afb7095c4cbb7fe9ce
luowengang <2628387671@qq.com>
2018-11-01 15:54:48 +0800

    增加hystrix-dashboard和turibne

--- hystrixdashboard/src/main/java/com/flyonsky/Application.java
+++ hystrixdashboard/src/main/java/com/flyonsky/Application.java
@@ -2,11 +2,14 @@ package com.flyonsky;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
+@EnableHystrixDashboard
 @SpringCloudApplication
 public class Application {
 
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(Application.class).run(args);
 	}
+
 }
