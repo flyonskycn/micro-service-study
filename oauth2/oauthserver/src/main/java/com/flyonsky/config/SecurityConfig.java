94435529947d7f6cb70ed9daf82b1f9a96c22bd1
luowengang <2628387671@qq.com>
2019-01-04 14:27:50 +0800

    权限控制

---
+++ oauth2/oauthserver/src/main/java/com/flyonsky/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.flyonsky.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Bean
+    public UserDetailsService userDetailsService(){
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withDefaultPasswordEncoder().username("clientapp").password("112233").roles("USER").build());
+        return manager;
+    }
+
+}
