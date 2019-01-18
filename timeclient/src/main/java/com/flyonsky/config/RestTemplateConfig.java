6fe2f8913b611ef63bef8f2eb2b1778e2d10915b
luowg <flyonskycn@gmail.com>
2019-01-18 23:23:08 +0800

    增加cat调用链监控

--- timeclient/src/main/java/com/flyonsky/config/RestTemplateConfig.java
+++ timeclient/src/main/java/com/flyonsky/config/RestTemplateConfig.java
@@ -1,16 +1,57 @@
 package com.flyonsky.config;
 
+import java.io.IOException;
+import java.lang.invoke.MethodHandles;
+import java.util.Collections;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.DefaultResponseErrorHandler;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class RestTemplateConfig {
+	
+	private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
 	@Bean
 	@LoadBalanced
 	public RestTemplate createRestTemplate() {
-		return new RestTemplate();
+		SimpleClientHttpRequestFactory clientHttpRequestFactory = new SimpleClientHttpRequestFactory();
+		clientHttpRequestFactory.setConnectTimeout(2000);
+		clientHttpRequestFactory.setReadTimeout(3000);
+		RestTemplate restTemplate = new RestTemplate(clientHttpRequestFactory);
+		
+		// 保存和传递调用链上下文
+		restTemplate.setInterceptors(Collections.singletonList(new CatRestInterceptor()));
+		
+		restTemplate.setErrorHandler(new DefaultResponseErrorHandler() {
+			@Override 
+			public boolean hasError(ClientHttpResponse response)
+					throws IOException {
+				try {
+					return super.hasError(response);
+				} catch (Exception e) {
+					return true;
+				}
+			}
+
+			@Override 
+			public void handleError(ClientHttpResponse response)
+					throws IOException {
+				try {
+					super.handleError(response);
+				} catch (Exception e) {
+					log.error("Exception [" + e.getMessage() + "] occurred while trying to send the request", e);
+					throw e;
+				}
+			}
+		});
+		return restTemplate;
 	}
 }
