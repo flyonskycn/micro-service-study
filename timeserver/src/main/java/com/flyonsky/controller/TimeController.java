3f46f14bd673c58dc3e3a2c5f6e1888b33092f5d
luowengang <2628387671@qq.com>
2019-02-13 11:18:24 +0800

    增加对spring-boot-starter-data-rest引入，实验jackson的时间全局格式化功能

--- timeserver/src/main/java/com/flyonsky/controller/TimeController.java
+++ timeserver/src/main/java/com/flyonsky/controller/TimeController.java
@@ -1,7 +1,13 @@
 package com.flyonsky.controller;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.flyonsky.service.TimeService;
@@ -19,6 +25,19 @@ public class TimeController {
 	@ApiOperation("时间函数")
 	public String currentime(){
 		return this.getTimeService().currentTime();
+	}
+	
+	@GetMapping("jsontime")
+	@ApiOperation("时间函数")
+	public Date jsonTime(){
+		return Calendar.getInstance().getTime();
+	}
+	
+	@PostMapping("totime")
+	@ApiOperation("时间函数")
+	public String toTime(@RequestBody Date time) {
+		SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		return sf.format(time);
 	}
 
 	public TimeService getTimeService() {
