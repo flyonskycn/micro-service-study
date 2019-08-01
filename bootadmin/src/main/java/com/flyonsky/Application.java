6f69847af111ac962870901b4880c3df5c402b36
luowg <flyonskycn@gmail.com>
2019-08-01 22:31:12 +0800

    增加boot admin项目引用

--- bootadmin/src/main/java/com/flyonsky/Application.java
+++ bootadmin/src/main/java/com/flyonsky/Application.java
@@ -1,8 +1,11 @@
 package com.flyonsky;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cloud.client.SpringCloudApplication;
 
+@SpringBootApplication
+@EnableAdminServer
 public class Application {
 
     public static void main(String[] args) {
