295c3f48cd70b70084b4e8eda2838aed31c595e6
luowengang <luowengang@ky-tech.com.cn>
2019-08-03 18:55:44 +0800

    增加Mockito学习代码

--- timeserver/src/test/java/com/flyonsky/mockito/MockitoTest.java
+++ timeserver/src/test/java/com/flyonsky/mockito/MockitoTest.java
@@ -9,6 +9,9 @@ import static org.mockito.Mockito.*;
 
 public class MockitoTest {
 
+    /**
+     * 验证某些行为
+     */
     @Test
     public void test1(){
         // mock creation 创建mock对象
@@ -23,6 +26,9 @@ public class MockitoTest {
         verify(mockedList).clear();
     }
 
+    /**
+     * 如何做一些测试桩 (Stub)
+     */
     @Test(expected = RuntimeException.class)
     public void test2(){
         // 你可以mock具体的类型,不仅只是接口
@@ -35,7 +41,6 @@ public class MockitoTest {
         // 输出“first”
         System.out.println(mockedList.get(0));
 
-        //following throws runtime exception
         // 抛出异常
         System.out.println(mockedList.get(1));
 
@@ -44,5 +49,61 @@ public class MockitoTest {
 
         // 验证get(0)被调用的次数
         verify(mockedList).get(0);
+    }
+
+    /**
+     * 参数匹配器 (matchers)
+     */
+    @Test
+    public void test3(){
+        // 你可以mock具体的类型,不仅只是接口
+        LinkedList mockedList = mock(LinkedList.class);
+
+        // 测试桩
+        when(mockedList.get(anyInt())).thenReturn("first");
+
+        System.out.println(mockedList.get(1));
+
+        verify(mockedList).get(1);
+    }
+
+    /**
+     * 验证函数的确切、最少、从未调用次数
+     */
+    @Test
+    public void test4(){
+        // 你可以mock具体的类型,不仅只是接口
+        LinkedList mockedList = mock(LinkedList.class);
+        //using mock
+        mockedList.add("once");
+
+        mockedList.add("twice");
+        mockedList.add("twice");
+
+        mockedList.add("three times");
+        mockedList.add("three times");
+        mockedList.add("three times");
+
+        mockedList.add("five times");
+        mockedList.add("five times");
+        mockedList.add("five times");
+        mockedList.add("five times");
+        mockedList.add("five times");
+
+        // 下面的两个验证函数效果一样,因为verify默认验证的就是times(1)
+        verify(mockedList).add("once");
+        verify(mockedList, times(1)).add("once");
+
+        // 验证具体的执行次数
+        verify(mockedList, times(2)).add("twice");
+        verify(mockedList, times(3)).add("three times");
+
+        // 使用never()进行验证,never相当于times(0)
+        verify(mockedList, never()).add("never happened");
+
+        // 使用atLeast()/atMost()
+        verify(mockedList, atLeastOnce()).add("three times");
+        verify(mockedList, atLeast(2)).add("five times");
+        verify(mockedList, atMost(5)).add("three times");
     }
 }
