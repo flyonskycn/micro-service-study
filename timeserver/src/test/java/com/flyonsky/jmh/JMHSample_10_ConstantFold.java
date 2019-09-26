046f0585d2c222ab5656cb296d98ced4afd711b3
luowengang <luowengang@ky-tech.com.cn>
2019-09-26 10:42:32 +0800

    添加jmh例子，了解jmh

--- timeserver/src/test/java/com/flyonsky/jmh/JMHSample_10_ConstantFold.java
+++ timeserver/src/test/java/com/flyonsky/jmh/JMHSample_10_ConstantFold.java
@@ -1,3 +1,33 @@
+/*
+ * Copyright (c) 2014, Oracle America, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ *  * Neither the name of Oracle nor the names of its contributors may be used
+ *    to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package com.flyonsky.jmh;
 
 import org.openjdk.jmh.annotations.*;
@@ -11,7 +41,19 @@ import java.util.concurrent.TimeUnit;
 @State(Scope.Thread)
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
-public class JMHSample10ConstantFold {
+public class JMHSample_10_ConstantFold {
+
+    /*
+     * The flip side of dead-code elimination is constant-folding.
+     *
+     * If JVM realizes the result of the computation is the same no matter what,
+     * it can cleverly optimize it. In our case, that means we can move the
+     * computation outside of the internal JMH loop.
+     *
+     * This can be prevented by always reading the inputs from non-final
+     * instance fields of @State objects, computing the result based on those
+     * values, and follow the rules to prevent DCE.
+     */
 
     // IDEs will say "Oh, you can convert this field to local variable". Don't. Trust. Them.
     // (While this is normally fine advice, it does not work in the context of measuring correctly.)
@@ -62,12 +104,14 @@ public class JMHSample10ConstantFold {
      *    (see the JMH homepage for possible caveats when running from IDE:
      *      http://openjdk.java.net/projects/code-tools/jmh/)
      */
+
     public static void main(String[] args) throws RunnerException {
         Options opt = new OptionsBuilder()
-                .include(JMHSample10ConstantFold.class.getSimpleName())
+                .include(JMHSample_10_ConstantFold.class.getSimpleName())
                 .forks(1)
                 .build();
 
         new Runner(opt).run();
     }
+
 }
